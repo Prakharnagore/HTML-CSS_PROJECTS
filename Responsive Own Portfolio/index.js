16b5aea97ffb947b8e90660ecdb269042a0d145c
Prakharnagore000 <prakhar@kloudone.com>
2022-03-12 13:29:07 +0530

    basix html and css projects

---
+++ Responsive Own Portfolio/index.js
@@ -0,0 +1,34 @@
+window.onscroll = function () {
+  myFunction();
+  scrollFunction();
+};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+  document.getElementById("curve1").style.top = scrolled * -10 + "%";
+  document.getElementById("curve2").style.top = scrolled * -5 + 10 + "%";
+}
+
+/*************************************************** */
+var mybutton = document.getElementById("myBtn");
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 500 ||
+    document.documentElement.scrollTop > 500
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
